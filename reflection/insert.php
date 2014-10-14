30c7e68de774aaebba50877e294efc1a02c08cd0
kkanclerz <kuba.kanclerz@creativestyle.pl>
2014-10-14 02:13:27 +0200

    mandrill api

--- reflection/insert.php
+++ reflection/insert.php
@@ -18,9 +18,38 @@
         $_SESSION['message'] = $message;
     }
 
+    function sendApiEmail($title, $reflection)
+    {
+        $url = 'https://mandrillapp.com/api/1.0/messages/send.json';
+        $params = [
+            'message' => array(
+                'subject' => $title,
+                'text' => $reflection,
+                'html' => '<p>'.$reflection.'</p>',
+                'from_email' => 'uek@no-replay.com',
+                'to' => array(
+                    array(
+                        'email' => 'jakub.kanclerz@gmail.com',
+                        'name' => 'Jakub'
+                    )
+                )
+            )
+        ];
+
+        $params['key'] = 'HEpZLrPrRBEa7W9fLAJKeQ';
+        $params = json_encode($params);
+        $ch = curl_init(); 
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+
+        $head = curl_exec($ch); 
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE); 
+        curl_close($ch); 
+    }
+
     $ref = $_SERVER['HTTP_REFERER'];
-
-
     $to      = 'jakub.kanclerz@gmail.com';
     $subject = 'the subject';
     $message = 'hello';
@@ -29,6 +58,8 @@
         'X-Mailer: PHP/' . phpversion();
 
     mail($to, $subject, $message, $headers);
+
+    sendApiEmail($_POST['title'], $_POST['reflection']);
 
     $con = mysqli_connect("127.0.0.1", "user", "pass", "baza");// Check connection
 
