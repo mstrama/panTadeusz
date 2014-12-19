b08162df7943912682e5cd60359522d5e8d57267
mstrama <marta_strama@wp.pl>
2014-12-20 00:32:01 +0100

    Update book-2.php

--- books/book-2.php
+++ books/book-2.php
@@ -1,8 +1,14 @@
-<div class="panel panel-default">
-  <div class="panel-body">
-    Księga Druka
-  </div>
-  <div class="panel-footer">Zamek</div>
+<div class="btn-group">
+  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+    Action <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
 </div>
 
 <p class="streszczenie">
